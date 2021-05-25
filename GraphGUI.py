a7dd00fbb5dd668d3fb07f82192ea5ac6b46ddfb
abarrios5 <a_barrios5@u.pacific.edu>
2021-05-25 16:54:07 -0700

    GUI to show of the different plots Using PyQT5

--- GraphGUI.py
+++ GraphGUI.py
@@ -1,27 +1,91 @@
-from tkinter import *
+# importing various libraries
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import random
 from PIL import ImageTk, Image
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-sample_data = pd.read_csv('California_Fire_Incidents.csv')
+# main window
+# which inherits QDialog
+class Window(QDialog):
 
-root = Tk()
-root.title("California")
-root.geometry("400x200")
+    # constructor
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that
+        # displays the 'figure'it takes the
+        # 'figure' instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # Just some button connected to 'plot' method ++++++++
+        self.button = QPushButton('Plot')
+        self.button1 = QPushButton('P')
+
+        # adding action to the button  ++++++++
+        self.button.clicked.connect(self.plot)
+        self.button1.clicked.connect(self.graph)
+
+        # creating a Vertical Box layout
+        layout = QVBoxLayout()
+
+        # adding tool bar to the layout
+        layout.addWidget(self.toolbar)
+
+        # adding canvas to the layout
+        layout.addWidget(self.canvas)
+
+        # adding push button to the layout ++++++++
+        layout.addWidget(self.button)
+        layout.addWidget(self.button1)
+        # setting layout to the main window
+        self.setLayout(layout)
+
+    # action called by thte push button
+    def plot(self):
+        # random data
+        data = [random.random() for i in range(10)]
+
+        # clearing old figure
+        self.figure.clear()
+
+        # create an axis
+        ax = self.figure.add_subplot(111)
+
+        # plot data
+        ax.plot(data, '*-')
+
+        # refresh canvas
+        self.canvas.draw()
+
+    def graph(self):
+        sample_data = pd.read_csv('California_Fire_Incidents.csv')
+        plt.hist(sample_data.AcresBurned, 50)
+        plt.show()
 
 
-def graph():
-    plt.hist(sample_data.AcresBurned, 50)
-    plt.show()
+# driver code
+if __name__ == '__main__':
+    # creating apyqt5 application
+    app = QApplication(sys.argv)
 
+    # creating a window object
+    main = Window()
 
-def gra():
-    plt.plot(sample_data.AcresBurned)
-    plt.show
+    # showing the window
+    main.show()
 
-
-my_button = Button(root, text="GraphIt", command=graph)
-my_button.pack()
-
-root.mainloop()
+    # loop
+    sys.exit(app.exec_())
